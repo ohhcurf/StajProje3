b7810677ed1edb90a43ec322cd80ce1927210cef
ohhcurf <ohhcurf@hotmail.com>
2023-10-06 22:38:46 +0300

    Proje dosyası ekle.

---
+++ StajProje3/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace StajProje3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            treeView = new TreeView();
+            BFSBTN = new Button();
+            DFSBTN = new Button();
+            DosyaYukleButton = new Button();
+            listBox = new ListBox();
+            SuspendLayout();
+            // 
+            // treeView
+            // 
+            treeView.Location = new Point(12, 12);
+            treeView.Name = "treeView";
+            treeView.Size = new Size(554, 417);
+            treeView.TabIndex = 0;
+            // 
+            // BFSBTN
+            // 
+            BFSBTN.Location = new Point(614, 27);
+            BFSBTN.Name = "BFSBTN";
+            BFSBTN.Size = new Size(137, 47);
+            BFSBTN.TabIndex = 1;
+            BFSBTN.Text = "BFS";
+            BFSBTN.UseVisualStyleBackColor = true;
+            BFSBTN.Click += BFSBTN_Click;
+            // 
+            // DFSBTN
+            // 
+            DFSBTN.Location = new Point(614, 96);
+            DFSBTN.Name = "DFSBTN";
+            DFSBTN.Size = new Size(137, 44);
+            DFSBTN.TabIndex = 2;
+            DFSBTN.Text = "DFS";
+            DFSBTN.UseVisualStyleBackColor = true;
+            DFSBTN.Click += DFSBTN_Click;
+            // 
+            // DosyaYukleButton
+            // 
+            DosyaYukleButton.Location = new Point(614, 380);
+            DosyaYukleButton.Name = "DosyaYukleButton";
+            DosyaYukleButton.Size = new Size(137, 49);
+            DosyaYukleButton.TabIndex = 3;
+            DosyaYukleButton.Text = "Dosya Yükle";
+            DosyaYukleButton.UseVisualStyleBackColor = true;
+            DosyaYukleButton.Click += FileUploadButton_Click;
+            // 
+            // listBox
+            // 
+            listBox.FormattingEnabled = true;
+            listBox.ItemHeight = 15;
+            listBox.Location = new Point(614, 160);
+            listBox.Name = "listBox";
+            listBox.Size = new Size(137, 199);
+            listBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox);
+            Controls.Add(DosyaYukleButton);
+            Controls.Add(DFSBTN);
+            Controls.Add(BFSBTN);
+            Controls.Add(treeView);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TreeView treeView;
+        private Button BFSBTN;
+        private Button DFSBTN;
+        private Button DosyaYukleButton;
+        private ListBox listBox;
+    }
+}
---
+++ StajProje3/Form1.cs
@@ -0,0 +1,269 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace StajProje3
+{
+    public partial class Form1 : Form
+    {
+        private TreeNode currentTraversalNode;
+        private TreeNode rootNode;
+        private Queue<TreeNode> bfsQueue;
+        private Stack<TreeNode> dfsStack;
+        private int bfsCurrentStep = 0;
+        private int dfsCurrentStep = 0;
+        private int totalNodes;
+        private bool bfsCompleted = false;
+        private bool dfsCompleted = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private TreeNode LoadTreeFromText(string text)
+        {
+            TreeNode root = null;
+            Dictionary<string, TreeNode> nodeDictionary = new Dictionary<string, TreeNode>();
+
+            string[] lines = text.Split('\n');
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(';');
+                string parentData = parts[0].Trim();
+                string[] childrenData = parts[1].Split(',').Select(s => s.Trim()).ToArray();
+
+                if (!nodeDictionary.TryGetValue(parentData, out TreeNode parentNode))
+                {
+                    parentNode = new TreeNode(parentData);
+                    nodeDictionary[parentData] = parentNode;
+
+                    if (root == null)
+                    {
+                        root = parentNode;
+                    }
+                }
+
+                foreach (string childData in childrenData)
+                {
+                    if (!nodeDictionary.TryGetValue(childData, out TreeNode childNode))
+                    {
+                        childNode = new TreeNode(childData);
+                        nodeDictionary[childData] = childNode;
+                    }
+
+                    parentNode.Nodes.Add(childNode);
+                }
+            }
+
+            return root;
+        }
+
+
+
+
+        private void LoadTreeView(TreeNode root)
+        {
+            treeView.Nodes.Clear();
+            treeView.Nodes.Add(root);
+        }
+
+
+
+
+        private void FileUploadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Metin Dosyalarý|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string filePath = openFileDialog.FileName;
+                    string fileContent = File.ReadAllText(filePath);
+                    rootNode = LoadTreeFromText(fileContent);
+                    LoadTreeView(rootNode);
+                    currentTraversalNode = rootNode;
+                    totalNodes = rootNode.GetNodeCount(true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Dosya yükleme sýrasýnda bir hata oluþtu.\n\nHata Detayý: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+
+        private void BFS(TreeNode root)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                TreeNode node = queue.Dequeue();
+                HighlightNode(node);
+                OutputNodeData(node.Text);
+                Application.DoEvents();
+                Thread.Sleep(500);
+
+                foreach (TreeNode child in node.Nodes)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+        }
+
+
+
+
+        private void DFS(TreeNode root)
+        {
+            if (root == null)
+            {
+                MessageBox.Show("Lütfen önce bir metin dosyasý yükleyin.");
+                return;
+            }
+
+            dfsStack = new Stack<TreeNode>();
+            dfsStack.Push(root);
+            DFSStep();
+        }
+
+
+
+
+        private void BFSBTN_Click(object sender, EventArgs e)
+        {
+            if (currentTraversalNode == null)
+            {
+                MessageBox.Show("Lütfen önce bir metin dosyasý yükleyin.");
+                return;
+            }
+
+            if (bfsCompleted)
+            {
+                listBox.Items.Clear();
+                bfsCompleted = false;
+            }
+
+            if (bfsCurrentStep == 0)
+            {
+                bfsQueue = new Queue<TreeNode>();
+                bfsQueue.Enqueue(currentTraversalNode);
+            }
+
+            BFSStep();
+            bfsCurrentStep++;
+        }
+
+
+
+        private void BFSStep()
+        {
+            if (bfsQueue.Count > 0)
+            {
+                TreeNode node = bfsQueue.Dequeue();
+                HighlightNode(node);
+                OutputNodeData(node.Text);
+
+                foreach (TreeNode child in node.Nodes)
+                {
+                    bfsQueue.Enqueue(child);
+                }
+
+                if (bfsQueue.Count == 0)
+                {
+                    MessageBox.Show("BFS iþlemi tamamlandý.");
+                    bfsCompleted = true;
+                }
+            }
+            else
+            {
+                bfsCompleted = true;
+            }
+        }
+
+
+
+        private void DFSBTN_Click(object sender, EventArgs e)
+        {
+            if (currentTraversalNode == null)
+            {
+                MessageBox.Show("Lütfen önce bir metin dosyasý yükleyin.");
+                return;
+            }
+
+            if (dfsCompleted)
+            {
+                listBox.Items.Clear();
+                dfsCompleted = false;
+            }
+
+            if (dfsCurrentStep == 0)
+            {
+                dfsStack = new Stack<TreeNode>();
+                dfsStack.Push(currentTraversalNode);
+            }
+
+            DFSStep();
+            dfsCurrentStep++;
+        }
+
+
+
+
+        private void DFSStep()
+        {
+            if (dfsStack.Count > 0)
+            {
+                TreeNode node = dfsStack.Pop();
+                HighlightNode(node);
+                OutputNodeData(node.Text);
+
+                foreach (TreeNode child in node.Nodes)
+                {
+                    dfsStack.Push(child);
+                }
+
+                if (dfsStack.Count == 0)
+                {
+                    MessageBox.Show("DFS iþlemi tamamlandý.");
+                    dfsCompleted = true;
+                }
+            }
+            else
+            {
+                dfsCompleted = true;
+            }
+        }
+
+
+
+
+        private void HighlightNode(TreeNode node)
+        {
+            treeView.SelectedNode = node;
+            treeView.Focus();
+        }
+
+
+
+
+        private void OutputNodeData(string data)
+        {
+            listBox.Items.Add(data);
+        }
+    }
+}
